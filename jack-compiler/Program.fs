1d015114014d10c37e57a5e8226227b79207fd6f
ephraim <ephraimungar6@gmail.com>
2024-06-18 22:14:33 +0300

    initial jottings

--- jack-compiler/Program.fs
+++ jack-compiler/Program.fs
@@ -1,8 +1,37 @@
 ﻿// Learn more about F# at http://fsharp.org
 
 open System
+open System.IO
+  
+let output = new StreamWriter("C:\\Users\\Ephy's Acer\\Downloads\\nand2tetris\\nand2tetris\\projects\\8\\FunctionCalls\\FibonacciElement\\FibonacciElement.asm")
+let mutable counter = 0
+
+    
+
+let doNothing()=()
+
+let GetFileName(filepath : string) = Path.GetFileNameWithoutExtension(filepath)
+    
 
 [<EntryPoint>]
 let main argv =
-    printfn "Hello World from F#!"
-    0 // return an integer exit code
+    printfn "Please enter the path: "
+    let path = Console.ReadLine()
+    let files = Directory.GetFiles(path, "*.jack")
+    counter <- 0
+    for file in files do
+        use reader =new StreamReader(file)
+        let className = GetFileName(file)
+        let mutable line = ""
+        while not (reader.EndOfStream) do
+            line <- reader.ReadLine()
+            let words = line.Split([|' '; '\t'|], StringSplitOptions.RemoveEmptyEntries)
+            if words.Length > 0 then
+               
+        let currentFileName = file
+        printf "End of input file: %s\n" currentFileName 
+    output.Flush()
+    output.Close() 
+    printf "Output files are ready"
+
+    0
--- jack-compiler/Tokenizer.fs
+++ jack-compiler/Tokenizer.fs
@@ -1,2 +1,71 @@
 ﻿module Tokenizer
 
+open System.Text
+open System.IO
+open System.Text.RegularExpressions
+
+// Define sets of keywords and symbols
+let keywords = 
+    Set.ofList ["class"; "constructor"; "function"; "method"; "field"; "static"; "var";
+                "int"; "char"; "boolean"; "void"; "true"; "false"; "null"; "this"; "let";
+                "do"; "if"; "else"; "while"; "return"]
+
+let symbols = 
+    Set.ofList ['{'; '}'; '('; ')'; '['; ']'; '.'; ','; ';'; '+'; '-'; '*'; 
+                '/'; '&'; '|'; '<'; '>'; '='; '~']
+
+// Function to check if a token is a keyword
+let isKeyword token = 
+    keywords.Contains(token)
+
+// Function to check if a token is a symbol
+let isSymbol token =
+    token.Length = 1 && symbols.Contains(token.[0])
+
+// Function to check if a token is an integer constant
+let isIntegerConstant token =
+    let success, value = Int32.TryParse(token)
+    success && value >= 0 && value <= 32767
+
+// Function to check if a token is a string constant
+let isStringConstant token =
+    let pattern = "^\"[^\n\"]*\"$"
+    Regex.IsMatch(token, pattern)
+
+// Function to check if a token is an identifier
+let isIdentifier token =
+    let pattern = "^[a-zA-Z_][a-zA-Z0-9_]*$"
+    Regex.IsMatch(token, pattern)
+
+
+// our class for defintion for the tokenizer
+type Tokenizer(filepath:string) =
+    let reader = new StreamReader(filepath)
+
+    let mutable currentToken : string option = None 
+
+    let advanceOnWhite (reader: StreamReader) = 
+        while not reader.EndOfStream && System.Char.IsWhiteSpace(char (reader.Peek())) do
+            reader.Read()
+
+    member this.hasMoreTokens() = 
+        not reader.EndOfStream
+
+    member this.advance() = 
+        let sb = StringBuilder()
+        let mutable nextChar = reader.Read()
+        advanceOnWhite(reader)
+        while nextChar <> -1 && not (System.Char.IsWhiteSpace(char nextChar)) do
+            sb.Append(char nextChar)
+            nextChar <- reader.Read()
+        let currentToken = sb.ToString()
+        currentToken
+
+
+
+
+
+
+
+
+
