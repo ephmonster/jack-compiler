844be62358de15976f5893c89e2d83e174b606b3
Eden Amiga <edenamiga@gmail.com>
2024-06-25 01:24:43 +0300

    refactored

--- jack-compiler/Tokenization/Tokenizer.fs
+++ jack-compiler/Tokenization/Tokenizer.fs
@@ -7,8 +7,7 @@ open System.Collections.Generic
 open Token
 
 let inline (>=<) a (b,c) = a >= b && a <= c
-let inline (+=) a b = a := a.Value + b
-let inline (+==) a b = a := a.Value + $"{b}" 
+let inline (+=) a b = a := a.Value + b 
 
 let Keywords =
     ["class"; "constructor"; "function"; "method"; "field"; 
@@ -26,155 +25,129 @@ let Ops =
 let UniaryOps = 
     ['-'; '~'] |> Set
 
-
-type Tokenizer(filepath: string) = 
-    class
-        let mutable _tokens = new List<Token>()
-        let _reader = File.ReadAllText(filepath)
-        let mutable _lines = File.ReadAllText(filepath).Split('\n')
-        let mutable _curToken: Token = Token("null","null")
-        let mutable current_word = ""
-        let mutable position = ref 0
-
-        member _.NextChar() =
-            let next_char = _reader[position.Value]
-            position += 1
-            next_char
+let isKeyword(word: string) = 
+        Keywords.Contains(word)
         
-        member _.hasMoreTokens() = 
-            position.Value < _reader.Length
+let isSymbol(word:string) =
+    word.Length = 1 && Symbols.Contains(char word)
 
-        // member this.Advance() = 
-        //     let mutable str = ""
-        //     let mutable ch = this.NextChar()
-        //     this.Skip()
-        //     while ch <> -1 && not(System.Char.IsWhiteSpace(char ch)) do
-        //         str <- str + $"{ch}"
-        //         ch <- _reader.Read()
-        //     _curToken = str
+let isIntegerConstant(word: string) = 
+    let success, value = Int32.TryParse(word)
+    success && value >=< (0, 32767) // value between 0 and 2^15
 
-        // Will return a list of tokens 
-        member this.Tokenize() = 
-            let mutable i = 0
-            let mutable lines = ResizeArray<string>()
-            while i < _lines.Length do
-                let mutable curLine = Regex.Replace(Regex.Replace(_lines[i].Trim(), @"\/\/.*$", ""), @"\/\*.*?\*\/", "");
-                let mutable x = "a"
-                if String.length(curLine) = 0 || curLine.StartsWith "//" ||
-                    curLine.StartsWith "\r" then
-                    i <- i + 1
-                elif curLine.StartsWith "/*" then
-                    while not(_lines[i].EndsWith "*/\r") do   
-                        i <- i + 1
-                    i <- i + 1
-                else 
-                    i <- i + 1
-                    lines.Add(curLine)
+let isStringConstant(word: string) =
+    Regex.IsMatch(word, "^\"[^\n\"]*\"$")
 
-            let mutable chars = ResizeArray<char>()
-            for i in lines.ToArray() do
-                let x = Regex.Replace($"{i}", @"\\n", "")
-                chars.AddRange (x.Trim().ToCharArray())
+let isIdentifier(word: string) =
+    Regex.IsMatch(word, "^[a-zA-Z_][a-zA-Z0-9_]*$")
 
-            for i in chars.ToArray() do
-                printf $"{i}"
 
-            (* Start of the actual analysis *)
-            let mutable chars = chars.ToArray()
-            let position = ref 0
+type Tokenizer(filepath: string) as self = 
+    let mutable _tokens = new List<Token>()
+    let mutable _curToken: Token = Token("null","null")
+    let mutable _chars: char array = Array.empty  
+    let mutable _position = ref -1
+    do 
+        self.Tokenizer(filepath)
 
-            while position.Value < chars.Length do
-                let mutable current_char = chars[position.Value]
-                
-                // whitespace
-                if current_char = ' ' then 
-                    position += 1
+    member _.Tokenizer(filepath:string) =
+        let i = ref 0
+        let mutable input = File.ReadAllText(filepath).Split('\n')
+        let mutable lines = ResizeArray<string>()
+        while i.Value < input.Length do
+            let mutable curLine = Regex.Replace(Regex.Replace(input[i.Value].Trim(), @"\/\/.*$", ""), @"\/\*.*?\*\/", "");
+            if String.length(curLine) = 0 || curLine.StartsWith "//" ||
+                curLine.StartsWith "\r" then
+                i += 1
+            elif curLine.StartsWith "/*" then
+                while not(input[i.Value].EndsWith("*/\r")) do   
+                    i += 1
+                i += 1
+            else 
+                i += 1
+                lines.Add(curLine)
 
-                // keywords or identifiers
-                elif Char.IsLetter(current_char) || current_char = '_' then
-                    current_word <- current_word + $"{current_char}"
-                    position += 1
-                    let mutable next_char = chars[position.Value]
-                    while position.Value < chars.Length && 
-                        (Char.IsLetterOrDigit(next_char) || next_char = '_') do
-                        if Char.IsLetter(next_char) then
-                            position += 1
-                            current_word <- current_word + $"{next_char}"
-                            next_char <- chars[position.Value]
-                            
-                    // reserved keywords
-                    if Tokenizer.isKeyword(current_word) then
-                        _curToken <- Token("keyword", current_word)
-                        _tokens.Add(_curToken)
-                        if current_word = "null" then
-                            ()
-                    // identifier 
-                    elif Tokenizer.isIdentifier(current_word) then
-                        _curToken <- Token("identifier", current_word)
-                        _tokens.Add(_curToken)
-                    else exit(-1)
-                    current_word <- ""
-                    printfn "%A" _curToken
-                
-                // integers
-                elif Char.IsDigit(current_char) then
-                    current_word <- current_word + $"{current_char}"
-                    position += 1
-                    let mutable next_char = chars[position.Value]
-                    while position.Value < chars.Length && Char.IsDigit(next_char) do
-                        position += 1
-                        current_word <- current_word + $"{next_char}"
-                        next_char <- chars[position.Value]
-                    if Tokenizer.isIntegerConstant(current_word) then
-                        _curToken <- Token("integerConstant", current_word)
-                        _tokens.Add(_curToken)
-                    else exit(-1)
-                    current_word <- ""
-                    printfn "%A" _curToken
-                
-                // strings
-                elif current_char = '"' then
-                    position += 1
-                    let mutable next_char = chars[position.Value]
-                    while position.Value < chars.Length && next_char <> '"' do
-                        position += 1
-                        current_word <- current_word + $"{next_char}"
-                        next_char <- chars[position.Value]
-                     
-                    // might have to check for next char
-                    _curToken <- Token("stringConstant", current_word)
-                    _tokens.Add(_curToken)
-                    current_word <- ""
-                    position += 1 // since cur_char = ", skip
-                    printfn "%A" _curToken
+        let mutable chars = ResizeArray<char>()
+        for i in lines.ToArray() do
+            let x = Regex.Replace($"{i}", @"\\n", "")
+            chars.AddRange(x.Trim().ToCharArray())
 
-                elif Symbols.Contains(current_char) then
-                    _curToken <- Token("symbol", $"{current_char}")
-                    _tokens.Add(_curToken)
-                    position += 1
-                    printfn "%A" _curToken
-                else exit(-1)
+        _chars <- chars.ToArray()
 
-            _tokens
-                
-                
-        static member isKeyword(token: string) = 
-            Keywords.Contains(token)
+    member _.NextChar() =
+        _position += 1
+        _chars[_position.Value]
+
+    member _.Peek() =
+        _chars[_position.Value + 1]
+    
+    member _.hasMoreTokens() = 
+        _position.Value + 1 < _chars.Length
+
+    // Also prints the token to console
+    member _.AddToken(token:Token) =
+        _curToken <- token        
+        _tokens.Add(token)
+        printfn "%A" token
+        
+    member this.Advance() = 
+        let mutable current_word = ""
+        let mutable current_char = this.NextChar()
+        let mutable next_char = char 0
+
+        // whitespace
+        if current_char = ' ' then 
+            this.Advance()   
+
+        // keywords or identifiers
+        elif Char.IsLetter(current_char) || current_char = '_' then
+            current_word <- current_word + $"{current_char}"
+
+            while this.hasMoreTokens() && (Char.IsLetterOrDigit(this.Peek()) || this.Peek() = '_') do
+                next_char <- this.NextChar()
+                current_word <- current_word + $"{next_char}"
+                    
+            // reserved keywords
+            if isKeyword(current_word) then
+                this.AddToken(Token("keyword", current_word))
+            // identifier 
+            elif isIdentifier(current_word) then
+                this.AddToken(Token("identifier", current_word))
+            else exit(-1)
+        
+        // integers
+        elif Char.IsDigit(current_char) then
+            current_word <- current_word + $"{current_char}"
+
+            while this.hasMoreTokens() && Char.IsDigit(this.Peek()) do
+                next_char <- this.NextChar()
+                current_word <- current_word + $"{next_char}"
+
+            if isIntegerConstant(current_word) then
+                this.AddToken(Token("integerConstant", current_word))
+            else exit(-1)
+        
+        // strings
+        elif current_char = '"' then
+            let mutable next_char = this.NextChar()
             
-        member this.isSymbol() =
-            String.length(current_word) = 1 && Symbols.Contains(char current_word)
+            while this.hasMoreTokens() && next_char <> '"' do
+                current_word <- current_word + $"{next_char}"
+                next_char <- this.NextChar()
+            
+            this.AddToken(Token("stringConstant", current_word))
 
-        static member isIntegerConstant(token: string) = 
-            let success, value = Int32.TryParse(token)
-            success && value >=< (0, 32767) // value between 0 and 2^15
+        elif Symbols.Contains(current_char) then
+            this.AddToken(Token("symbol", $"{current_char}"))
 
-        static member isStringConstant(token: string) =
-            Regex.IsMatch(token, "^\"[^\n\"]*\"$")
+        else exit(-1)
 
-        static member isIdentifier(token: string) =
-            Regex.IsMatch(token, "^[a-zA-Z_][a-zA-Z0-9_]*$")
-    end
+    // get tokens
+    member this.Tokenize() =
+        while this.hasMoreTokens() do
+            this.Advance()
 
+        _tokens
 
 
 
