54133af3fc36718c6abd23205693badde7a53300
Eden Amiga <edenamiga@gmail.com>
2024-06-26 18:13:04 +0300

    changed file ordering

--- jack-compiler/Compiler.fs
+++ jack-compiler/Compiler.fs
@@ -1,19 +1,20 @@
 ﻿open System.IO
-open Tokenizer
 open XmlWriter
-  
+open Tokenizer
+open CompilationEngine
+
 let INPUT_DIR = Path.Combine(Directory.GetParent(__SOURCE_DIRECTORY__).FullName, "jack-compiler\\Input\\")
 let OUTPUT_DIR = Path.Combine(Directory.GetParent(__SOURCE_DIRECTORY__).FullName, "jack-compiler\\Output\\")
-
-let FileName(filepath : string) = Path.GetFileNameWithoutExtension(filepath)    
+let FileName(fpath:string) = Path.GetFileNameWithoutExtension(fpath)    
 
 [<EntryPoint>]
 let main _ =
     let files = Directory.GetFiles(INPUT_DIR, "*.jack")
     for file in files do
         let className = FileName(file)
-        let tokens = Tokenizer(file).Tokenize()
-        XmlWriter(OUTPUT_DIR, className, tokens)         
+        XmlWriter(OUTPUT_DIR, className, Tokenizer(file).Tokenize()) 
+        CompilationEngine(file, $"{OUTPUT_DIR}{className}.xml").compileClass()
         printfn $"End of input file: {file}"  
+    
     printfn "Output files are ready"
     0
