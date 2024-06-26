860154c3dada05a1a0c5d012669faa8640b23e1b
Eden Amiga <71780707+edenpro2@users.noreply.github.com>
2024-06-26 18:03:39 +0300

    Delete jack-compiler/Tokenization directory

--- jack-compiler/Tokenization/Token.fs
+++
@@ -1,7 +0,0 @@
-module Token
-[<StructuredFormatDisplay("<{Type}>{Lexeme}</{Type}>")>]
-type public Token(typ:string, lex:string) = 
-    member val Type = typ 
-    member val Lexeme = lex
-    new() = Token(null, null)
-    
